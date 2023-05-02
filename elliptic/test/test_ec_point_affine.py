573edbb2008dee5bd127bb2ef9c57af87deae240
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-05-02 19:08:07 +0200

    Introduce WeierstrassCurve

--- elliptic/test/test_ec_point_affine.py
+++ elliptic/test/test_ec_point_affine.py
@@ -2,25 +2,33 @@ import unittest
 
 from elliptic.crypto.ec_point_affine import ECPointAffine
 from elliptic.crypto.field import Field
+from elliptic.crypto.weierstrass_curve import Secp256k1_13
 
 class TestECPointAffine(unittest.TestCase):
+    curve = Secp256k1_13
+    p = 0
+
+    def setup(self):
+        self.p = self.curve.p
+
     def test_init_with_int(self):
-        p = ECPointAffine(1, 2, 13)
+        p = ECPointAffine(1, 2, self.curve)
         self.assertEqual(p.x, Field(1, 13))
         self.assertEqual(p.y, Field(2, 13))
 
     def test_init_with_field(self):
-        p = ECPointAffine(Field(1,13), Field(2,13))
+        p = ECPointAffine(Field(1,13), Field(2,13), self.curve)
         self.assertEqual(p.x, Field(1, 13))
         self.assertEqual(p.y, Field(2, 13))
 
     def test_eq(self):
-        self.assertTrue(ECPointAffine(1, 2, 13) == ECPointAffine(1, 2, 13))
-        self.assertFalse(ECPointAffine(1, 2, 13) == ECPointAffine(2, 1, 13))
+        curve = self.curve
+        self.assertTrue(ECPointAffine(1, 2, curve) == ECPointAffine(1, 2, curve))
+        self.assertFalse(ECPointAffine(1, 2, curve) == ECPointAffine(2, 1, curve))
 
     def test_neg(self):
-        p = ECPointAffine(1, 2, 13)
-        self.assertEqual(-p, ECPointAffine(1, 11, 13))
+        p = ECPointAffine(1, 2, self.curve)
+        self.assertEqual(-p, ECPointAffine(1, 11, self.curve))
 
     def test_double_neg(self):
         for i in range(1, 13):
@@ -28,9 +36,9 @@ class TestECPointAffine(unittest.TestCase):
             self.assertEqual(--p, p)
 
     def test_add(self):
-        p = ECPointAffine(1, 2, 13)
-        q = ECPointAffine(2, 1, 13)
-        self.assertEqual(p + q, ECPointAffine(11, 8, 13))
+        p = ECPointAffine(1, 2, self.curve)
+        q = ECPointAffine(2, 1, self.curve)
+        self.assertEqual(p + q, ECPointAffine(11, 8, self.curve))
 
 if __name__ == "__main__":
     unittest.main()
