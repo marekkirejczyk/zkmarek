848b22ced46e4aabde8132403540b73006fd83ff
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-05-13 10:11:19 +0200

    fix linter

--- elliptic/crypto/weierstrass_curve.py
+++ elliptic/crypto/weierstrass_curve.py
@@ -1,3 +1,6 @@
+from elliptic.crypto.ec_point_affine import ECPointAffine
+
+
 class WeierstrassCurve:
     p = None
     a = None
@@ -8,11 +11,14 @@ class WeierstrassCurve:
         self.b = b
         self.p = p
 
-    def evaluate_at(self, point: "ECPointAffine"):
-      return (point.y.value**2 - point.x.value**3 - self.a * point.x.value - self.b) % self.p
+    def evaluate_at(self, point: ECPointAffine):
+        return (
+            point.y.value**2 - point.x.value**3 - self.a * point.x.value - self.b
+        ) % self.p
 
     def __eq__(self, other):
         return self.a == other.a and self.b == other.b and self.p == other.p
+
 
 Secp256k_Order = 2**256 - 2**32 - 2**9 - 2**8 - 2**7 - 2**6 - 2**4 - 1
 Secp256k1 = WeierstrassCurve(0, 7, Secp256k_Order)
