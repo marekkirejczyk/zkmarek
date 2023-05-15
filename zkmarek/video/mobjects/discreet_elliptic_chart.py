a391e4d82481f75942b396a61af1d8bc109b3b2f
Marek Kirejczyk <marekkirejczyk@users.noreply.github.com>
2023-05-15 12:48:35 +0200

    Refactor DiscreetEllipticChart to use generate_points() (#18)
    * Refactor DiscreetEllipticChart to use generate_points()
    
    * Remove unused code

--- zkmarek/video/mobjects/discreet_elliptic_chart.py
+++ zkmarek/video/mobjects/discreet_elliptic_chart.py
@@ -1,26 +1,24 @@
 from manim import Axes, Tex, TexTemplate, VGroup, Dot, Create, FadeOut, YELLOW
-from zkmarek.crypto.field import Field
-
-
-def secp256k1(x, y):
-    return y**2 - x**3 - 7
+from zkmarek.crypto.ec_point_affine import ECPointAffine
+from zkmarek.crypto.weierstrass_curve import Secp256k1_41, WeierstrassCurve
 
 
 class DiscreetEllipticChart(VGroup):
-    def __init__(self, prime=19, equation=secp256k1):
+    curve: WeierstrassCurve
+
+    def __init__(self, curve=Secp256k1_41):
         VGroup.__init__(self)
-        self.equation = equation
-        self.prime = prime
+        self.curve = curve
 
         self.ax = Axes(
-            x_range=[0, prime + 1, 5],
-            y_range=[0, prime + 1, 2],
+            x_range=[0, curve.p + 1, 5],
+            y_range=[0, curve.p + 1, 5],
             x_length=7,
             axis_config={"include_numbers": True},
         )
         template = TexTemplate()
         template.add_to_preamble(r"\usepackage{amsfonts}")
-        field_label = r"$\mathbb{F}_{" + str(prime) + "}$"
+        field_label = r"$\mathbb{F}_{" + str(curve.p) + "}$"
         self.labels = self.ax.get_axis_labels(
             Tex(field_label, tex_template=template, font_size=26),
             Tex(field_label, tex_template=template, font_size=26),
@@ -30,10 +28,9 @@ class DiscreetEllipticChart(VGroup):
         self.gen_points()
 
     def gen_points(self):
-        p = self.prime
-        for x, y in [(x, y) for x in range(0, p) for y in range(0, p)]:
-            if self.equation(Field(x, p), Field(y, p)) == Field(0, self.prime):
-                self.add(Dot(self.ax.c2p(x, y), color=YELLOW))
+        points = ECPointAffine.generate_points(self.curve)
+        for p in points:
+            self.add(Dot(self.ax.c2p(p.x.value, p.y.value), color=YELLOW))
 
     def animate_appear(self):
         return Create(self)
