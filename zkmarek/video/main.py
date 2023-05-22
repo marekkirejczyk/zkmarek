63fa93e88f1321ea1f749bb83cc3f441a3da894d
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-05-22 15:17:46 +0200

    Render subset of slides specified by env var SLIDES

--- zkmarek/video/main.py
+++ zkmarek/video/main.py
@@ -1,36 +1,40 @@
 import os
 
 from manim import Scene, config
-
 from presentation import Presentation
-from slides.title import TitleSlide
-from slides.equation import EquationSlide
-from slides.cec.addition import Addition
+from slides.cec.addition import Addition as CECAddition
 from slides.cec.negation import Negation as CECNegation
 from slides.cec.operations import Operations as CECOperations
-from slides.ec.negation import Negation as ECNegation
 from slides.ec.addition import Addition as ECAddition
+from slides.ec.negation import Negation as ECNegation
+from slides.equation import EquationSlide
+from slides.title import TitleSlide
+from zkmarek.video.slides.slide_base import SlideBase
 
-from zkmarek.video.slides.test_slide import TestSlide # noqa: F401 # pyright: ignore
+from zkmarek.video.slides.test_slide import TestSlide
+from zkmarek.video.utils import get_slides_from_names # noqa: F401 # pyright: ignore
 
 config.width = 16
 config.height = 9
 
+SLIDES = [
+    TitleSlide("Elliptic Curves"),
+    EquationSlide(),
+    CECOperations(),
+    CECNegation(),
+    CECAddition(),
+    TitleSlide("Discrete Elliptic Curves"),
+    ECNegation(),
+    ECAddition(),
+]
+
+
 class EllipticCurves(Scene):
     def construct(self):
-        if "MANIM_SLIDE" in os.environ:
-            cls_name = os.environ["MANIM_SLIDE"]
-            cls = globals()[cls_name]()
-            scenes = [cls]
-        else:
-            scenes = [
-                TitleSlide("Elliptic Curves"),
-                EquationSlide(),
-                CECOperations(),
-                CECNegation(),
-                Addition(),
-                TitleSlide("Discrete Elliptic Curves"),
-                ECNegation(),
-                ECAddition(),
-            ]
-        Presentation(self).play(scenes)
+        env_slides = get_slides_from_names(dict(os.environ).get("SLIDES"), globals())
+        slides = slides = SLIDES if env_slides is None else env_slides
+
+        print("Rendering slides: ")
+        SlideBase.print(slides)
+
+        Presentation(self).play(slides)
--- zkmarek/video/utils.py
+++ zkmarek/video/utils.py
@@ -1,4 +1,12 @@
+from typing import Optional
+from slides.equation import SlideBase
+
 def load(filename):
     with open(filename, 'r') as file:
         data = file.read()
     return data
+
+def get_slides_from_names(slide_names, globals=globals()):
+    if slide_names is None:
+        return None
+    return [globals[name.strip()]() for name in slide_names.split(",")]
