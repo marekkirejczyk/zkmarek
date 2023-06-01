d1c4288b9857c36d95e43bd5aec9c5815714f1f1
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-06-01 20:46:54 +0200

    Layout nicely

--- zkmarek/video/slides/ec/standards.py
+++ zkmarek/video/slides/ec/standards.py
@@ -1,4 +1,4 @@
-from manim import FadeIn, FadeOut, MathTex, MobjectTable, UP, VGroup, LEFT, RIGHT
+from manim import FadeIn, FadeOut, MathTex, MobjectTable, UP, VGroup, LEFT, Text, DOWN
 
 from zkmarek.video.slides.common.slide_base import SlideBase
 
@@ -34,6 +34,9 @@ class Standard(MobjectTable):
 class Standards(SlideBase):
     secp256k1: Standard
     curve25519: Standard
+    alt_BN128: Standard
+    label: Text
+    group: VGroup
 
     def __init__(self):
         super().__init__("Standards")
@@ -56,11 +59,13 @@ class Standards(SlideBase):
             " {2^{252}+27742317777372353535851937790883648493",
             "h = 8",
         )
-        self.secp256k1.align_on_border(UP + LEFT)
-        self.curve25519.align_on_border(UP + RIGHT)
-
+        self.label = Text(self.title)
+        self.label.to_edge(UP)
+        self.group = VGroup(self.secp256k1, self.curve25519).arrange_in_grid(cols=2)
+        self.group.next_to(self.label, DOWN)
 
     def animate_in(self, scene):
+        scene.play(FadeIn(self.label))
         scene.play(FadeIn(self.secp256k1))
         scene.play(FadeIn(self.curve25519))
 
