8094d2b47a433377b6441755c33fa2a120fdada0
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-06-01 20:34:24 +0200

    Add Curve25519 standard

--- zkmarek/video/slides/ec/standards.py
+++ zkmarek/video/slides/ec/standards.py
@@ -1,32 +1,19 @@
-from manim import FadeIn, FadeOut, MathTex, MobjectTable, UP, VGroup, LEFT
+from manim import FadeIn, FadeOut, MathTex, MobjectTable, UP, VGroup, LEFT, RIGHT
 
 from zkmarek.video.slides.common.slide_base import SlideBase
 
 
-class Standards(SlideBase):
-    secp256k1: MobjectTable
+class Standard(MobjectTable):
 
-    def __init__(self):
-        super().__init__("Standards")
-        self.title = "Standards"
-
-    def create_cell(self, caption, val):
-        label = MathTex(caption, font_size=16)
-        value = MathTex(val, font_size=26)
-        label.next_to(value, UP, buff=0.1)
-        label.align_to(value, LEFT)
-        value.align_to(value, LEFT)
-        return VGroup(label, value)
-
-    def create_standard(self, name, equation, p, g, n, h):
-        return MobjectTable(
+    def __init__(self, name, equation, p, g, n, h):
+        super().__init__(
             [
-                [self.create_cell("Name", name)],
-                [self.create_cell("Equation", equation)],
-                [self.create_cell("Order\,p =", p)],
-                [self.create_cell("Base\,point\,G =", g)],
-                [self.create_cell("Order\,of\,G\,n=", n)],
-                [self.create_cell("Cofactor", h)],
+                [Standard.create_cell("Name", name)],
+                [Standard.create_cell("Equation", equation)],
+                [Standard.create_cell("Order\,p =", p)],
+                [Standard.create_cell("Base\,point\,G_x =", g)],
+                [Standard.create_cell("Order\,of\,G\,n=", n)],
+                [Standard.create_cell("Cofactor", h)],
             ],
             include_outer_lines=True,
             v_buff=0.2,
@@ -35,20 +22,47 @@ class Standards(SlideBase):
             line_config={"stroke_width": 1}
         )
 
+    @staticmethod
+    def create_cell(caption, val):
+        label = MathTex(caption, font_size=16)
+        value = MathTex(val, font_size=26)
+        label.next_to(value, UP, buff=0.1)
+        label.align_to(value, LEFT)
+        value.align_to(value, LEFT)
+        return VGroup(label, value)
+
+class Standards(SlideBase):
+    secp256k1: Standard
+    curve25519: Standard
+
+    def __init__(self):
+        super().__init__("Standards")
+        self.title = "Standards"
+
     def construct(self):
-        self.secp256k1 = self.create_standard(
+        self.secp256k1 = Standard(
             "Secp256k1",
             "y^2 = x^3 + 7",
             "p = 2^{256} - 2^{32} - 2^9 - 2^8 - 2^7 - 2^6 - 2^4 - 1",
             "02\enspace 79BE667E\enspace F9DCBBAC\enspace ... \enspace D0364141",
             "FFFFFFFF\enspace FFFFFFFF\enspace ...\enspace BFD25E8C",
-            "h = 01",
+            "h = 1",
+        )
+        self.curve25519 = Standard(
+            "Curve25519",
+            "y^2 = x^3 + 486662x^2 + x",
+            "p = 2^{255}-19",
+            "G_x = 9",
+            " {2^{252}+27742317777372353535851937790883648493",
+            "h = 8",
         )
         self.secp256k1.align_on_border(UP + LEFT)
+        self.curve25519.align_on_border(UP + RIGHT)
 
 
     def animate_in(self, scene):
         scene.play(FadeIn(self.secp256k1))
+        scene.play(FadeIn(self.curve25519))
 
     def animate_out(self, scene):
         scene.play(FadeOut(self.secp256k1))
