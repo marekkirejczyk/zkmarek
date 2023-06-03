6048582175eb772574ef3188de41f90cdf8a782f
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-06-03 12:50:06 +0200

    cleanup

--- zkmarek/video/slides/ec/signature.py
+++ zkmarek/video/slides/ec/signature.py
@@ -1,5 +1,4 @@
-from manim import DOWN, GREEN, RED, FadeIn, FadeOut, MathTex, Text, Transform, VGroup
-from manimpango import list_fonts
+from manim import DOWN, GREEN, RED, FadeIn, FadeOut, Text, Transform
 
 from zkmarek.video.slides.common.slide_base import SlideBase
 
@@ -16,7 +15,6 @@ class Signature(SlideBase):
         self.private_key = Text("⚿ Private key", color=RED)
         self.public_key = Text("⚿ Public key", color=GREEN)
         self.public_key.next_to(self.private_key, DOWN)
-        print(list_fonts())
 
     def animate_in(self, scene):
         scene.play(FadeIn(self.public_key), FadeIn(self.private_key))
@@ -31,4 +29,3 @@ class Signature(SlideBase):
 
     def animate_out(self, scene):
         scene.play(FadeOut(self.public_key))
-
