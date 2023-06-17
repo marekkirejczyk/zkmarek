2463b27dc5728cc52318436c0cd27a01cf899850
Marek Kirejczyk <marekkirejczyk@users.noreply.github.com>
2023-06-17 19:44:36 +0200

    PrimeField animations cont. (#54)
    * Clock hand rotations
    * Morph clock to use zero and to have 41 elements

--- zkmarek/video/mobjects/clock.py
+++ zkmarek/video/mobjects/clock.py
@@ -1,5 +1,7 @@
+
 import numpy as np
 from manim import GREEN, ORIGIN, DOWN, Circle, Create, Line, VGroup, Text, UP
+from typing import Optional
 
 
 class Clock(VGroup):
@@ -8,27 +10,41 @@ class Clock(VGroup):
     use_zero: bool
     modulus: int
     radius: float
+    hour: Optional[float]
+    hour_hand: Optional[Line] = None
 
-    def __init__(self, use_zero=True, modulus=12):
+    def __init__(self, use_zero=False, modulus=12, hour = None):
         super().__init__()
         self.use_zero = use_zero
         self.modulus = modulus
         self.circle = Circle(radius=3)
-        self.markings = self.create_markings(12)
+        self.hour = hour
+        self.markings = self.create_markings(modulus)
+        if hour is not None:
+            self.hour_hand = self.create_hand(hour)
+            self.add(self.hour_hand)
         self.add(self.circle)
         self.add(self.markings)
 
+
     def label_at(self, index):
-        if self.use_zero and index == 0:
+        if index == 0 and not self.use_zero:
             return str(self.modulus)
-        return str(index % 12)
+        return str(index % self.modulus)
+
+    def create_hand(self, hour, length=2):
+        angle = hour * 360 / self.modulus
+        line = Line(ORIGIN, length*UP)
+        line.rotate(-np.deg2rad(angle), about_point=ORIGIN)
+        return line
+
 
     def create_markings(self, n):
         result = VGroup()
         for i in range(n):
             angle = i * 360 / n
             line = Line(2.8 * UP, 3 * UP, color=GREEN)
-            number = Text(self.label_at(i)).scale(0.5)
+            number = Text(self.label_at(i)).scale(0.4)
             number.next_to(line.get_center(), direction=DOWN, buff=0.5)
             line.rotate(-np.deg2rad(angle), about_point=ORIGIN)
             number.rotate(-np.deg2rad(angle), about_point=ORIGIN)
@@ -39,3 +55,5 @@ class Clock(VGroup):
     def animate_in(self, scene):
         scene.play(Create(self.circle))
         scene.play(Create(self.markings))
+        if self.hour_hand:
+            scene.play(Create(self.hour_hand))
--- zkmarek/video/slides/prime_fields.py
+++ zkmarek/video/slides/prime_fields.py
@@ -1,21 +1,47 @@
-from manim import LEFT
+from typing import Optional
+
+from manim import LEFT, FadeIn, FadeOut, Transform
 from zkmarek.video.mobjects.clock import Clock
+from zkmarek.video.mobjects.fill_angle import FillAngle
 from zkmarek.video.mobjects.sidebar import Sidebar
 
 from .common.slide_base import SlideBase
 
+
 class PrimeFields(SlideBase):
-    clock: Clock
+    clock: Optional[Clock] = None
     sidebar: Sidebar
 
     def __init__(self):
         super().__init__(title="Prime Fields")
 
-    def construct(self):
-        self.clock = Clock()
-        self.sidebar = Sidebar("Prime Field", code_path="data/pf/field.py")
-
     def animate_in(self, scene):
-        self.clock.animate_in(scene)
-        scene.play(self.clock.animate.align_on_border(LEFT, buff=0.2))
+        clock = Clock(hour=9)
+        clock.set_z_index(3)
+        clock.animate_in(scene)
+        angle = FillAngle(deg1=270, deg2=330)
+        scene.play(FadeIn(angle))
+
+        clock2 = Clock(hour=11)
+        scene.play(Transform(clock, clock2))
+        scene.play(FadeOut(angle))
+
+        angle2 = FillAngle(deg1=330, deg2=30)
+        scene.play(FadeIn(angle2))
+        clock3 = Clock(hour=1)
+        scene.remove(clock)
+        scene.play(Transform(clock2, clock3))
+        scene.play(FadeOut(angle2))
+
+        scene.remove(clock2)
+        clock4 = Clock(use_zero=True, hour=1)
+        scene.play(Transform(clock3, clock4))
+
+        scene.remove(clock3)
+        clock5 = Clock(use_zero=True, hour=4, modulus=41)
+        scene.play(Transform(clock4, clock5))
+
+        scene.remove(clock4)
+        scene.play(clock5.animate.align_on_border(LEFT, buff=0.2))
+        self.sidebar = Sidebar("Prime Field", code_path="data/pf/field.py")
         self.sidebar.animate_in(scene)
