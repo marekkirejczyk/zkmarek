1ef483d3cfdd457a5a03f022544ee186ebcd04b9
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-06-24 21:50:37 +0200

    Add animations to ECRecover slide

--- zkmarek/video/slides/eth/ecrecover.py
+++ zkmarek/video/slides/eth/ecrecover.py
@@ -1,7 +1,7 @@
-from manim import DOWN, UP, Code, FadeIn, Tex, Text
+from manim import DOWN, UP, Code, FadeIn, Tex, Text, Indicate
 
 from zkmarek.video.slides.common.slide_base import SlideBase
-from zkmarek.video.utils import load
+from zkmarek.video.utils import find_in_code, load
 
 
 class ECRecoverSlide(SlideBase):
@@ -27,6 +27,7 @@ class ECRecoverSlide(SlideBase):
         )
         self.docs = Tex(
             load("data/eth/ecrecover.tex"),
+            tex_environment=None,
             font_size=32)
         self.title_text.to_edge(UP)
         self.code.next_to(self.title_text, DOWN, buff=0.5)
@@ -36,3 +37,11 @@ class ECRecoverSlide(SlideBase):
         scene.play(FadeIn(self.title_text))
         scene.play(FadeIn(self.code))
         scene.play(FadeIn(self.docs))
+        fragments = ["bytes32 r", "bytes32 s", "uint8 v", ]
+
+        for fragment in fragments:
+            chars = find_in_code(self.code, fragment)
+            scene.play(Indicate(*chars))
+
+        for i in range(1, 8, 2):
+            scene.play(Indicate(self.docs[i]))
