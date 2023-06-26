0c1844be507ae621c26d9707575df6e20eacbc8d
Marek Kirejczyk <marekkirejczyk@users.noreply.github.com>
2023-06-26 21:42:29 +0200

    Various fixes and improvements (#68)
    * Fix typos in operations slides 
    
    * Add missing sections in dec addition to infinity
    
    * Nicer style for sidebar code
    
    * PF: clock with a high order 
    
    * PF: show inv function in code
    
    * PF: Fix z-index
    
    * PF: morph clock into an axis

--- zkmarek/video/slides/prime_fields.py
+++ zkmarek/video/slides/prime_fields.py
@@ -1,6 +1,8 @@
 from typing import Optional
 
-from manim import LEFT, FadeIn, FadeOut, Transform
+from manim import (DOWN, LEFT, RED, FadeIn, FadeOut, NumberLine,
+                   ReplacementTransform, TransformMatchingShapes)
+
 from zkmarek.video.mobjects.clock import Clock
 from zkmarek.video.mobjects.fill_angle import FillAngle
 from zkmarek.video.mobjects.sidebar import Sidebar
@@ -24,29 +26,45 @@ class PrimeFields(SlideBase):
         scene.play(FadeIn(angle))
 
         clock2 = Clock(hour=11)
-        scene.play(Transform(clock, clock2))
+        scene.play(ReplacementTransform(clock, clock2))
         scene.play(FadeOut(angle))
 
         self.new_subsection(scene, "Add 2 modulo")
         angle2 = FillAngle(deg1=330, deg2=30)
         scene.play(FadeIn(angle2))
         clock3 = Clock(hour=1)
-        scene.remove(clock)
-        scene.play(Transform(clock2, clock3))
+
+
+        scene.play(ReplacementTransform(clock2, clock3))
         scene.play(FadeOut(angle2))
 
-        scene.remove(clock2)
-        self.new_subsection(scene, "Add 2 modulo")
-        clock4 = Clock(use_zero=True, hour=1)
-        scene.play(Transform(clock3, clock4))
-
         self.new_subsection(scene, "Use zero")
-        scene.remove(clock3)
-        clock5 = Clock(use_zero=True, hour=4, modulus=41)
-        scene.play(Transform(clock4, clock5))
+        clock4 = Clock(use_zero=True, hour=1)
+        scene.play(ReplacementTransform(clock3, clock4))
+
+        self.new_subsection(scene, "Modulo 200")
+        clock5 = Clock(use_zero=True, hour=4, modulus=200)
+        scene.play(ReplacementTransform(clock4, clock5))
+        scene.wait(1)
 
         self.new_subsection(scene, "Modulo 41")
-        scene.remove(clock4)
-        scene.play(clock5.animate.align_on_border(LEFT, buff=0.2))
+        clock6 = Clock(use_zero=True, hour=4, modulus=41)
+        scene.play(ReplacementTransform(clock5, clock6))
+
+        self.new_subsection(scene, "Morph to line")
+        line = NumberLine(
+            x_range=[0, 41, 2],
+            length=10,
+            color=RED,
+            include_numbers=True,
+            label_direction=DOWN,
+            include_tip=True,
+        ).scale(0.6)
+        scene.play(TransformMatchingShapes(clock6, line))
+
+        self.new_subsection(scene, "Sidebar")
+        scene.play(line.animate.align_on_border(LEFT, buff=0.2))
         self.sidebar = Sidebar("Prime Field", code_path="data/pf/field.py")
         self.sidebar.animate_in(scene)
+
+
