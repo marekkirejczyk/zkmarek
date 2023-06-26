5d756b64b956bbdb4a8dfff76bd6c1e5338c1ec3
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-06-26 20:53:12 +0200

    PF with a high order and inv in code

--- data/pf/field.py
+++ data/pf/field.py
@@ -1,26 +1,27 @@
 class Field:
-    value: int
-    order: int
+    value: int # BigInteger
+    order: int # BigInteger
 
     def __init__(self, value: int, order: int):
         self.value = value % order
         self.order = order
 
-    def __eq__(self, other: "Field") -> bool:
+    def __eq__(self, other: Field) -> bool:
         assert self.order == other.order
         return self.value == other.value
 
-    def __add__(self, rhs: "FieldLike") -> "Field":
-        other = Field.create_from(rhs, self.order)
+    def __add__(self, other: Field) -> Field:
         assert self.order == other.order
         return Field(self.value + other.value, self.order)
 
-    def __sub__(self, rhs: "FieldLike") -> "Field":
-        other = Field.create_from(rhs, self.order)
+    def __sub__(self, other: Field) -> Field:
+
         assert self.order == other.order
         return Field(self.value - other.value, self.order)
 
-    def __mul__(self, rhs: "FieldLike") -> "Field":
-        other = Field.create_from(rhs, self.order)
+    def __mul__(self, other: Field) -> Field:
         assert self.order == other.order
         return Field(self.value * other.value, self.order)
+
+    def inv(self) -> Field:
+        return ...
--- zkmarek/video/slides/prime_fields.py
+++ zkmarek/video/slides/prime_fields.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from manim import LEFT, FadeIn, FadeOut, Transform
+from manim import LEFT, FadeIn, FadeOut, ReplacementTransform
 from zkmarek.video.mobjects.clock import Clock
 from zkmarek.video.mobjects.fill_angle import FillAngle
 from zkmarek.video.mobjects.sidebar import Sidebar
@@ -24,29 +24,34 @@ class PrimeFields(SlideBase):
         scene.play(FadeIn(angle))
 
         clock2 = Clock(hour=11)
-        scene.play(Transform(clock, clock2))
+        scene.play(ReplacementTransform(clock, clock2))
         scene.play(FadeOut(angle))
 
         self.new_subsection(scene, "Add 2 modulo")
         angle2 = FillAngle(deg1=330, deg2=30)
         scene.play(FadeIn(angle2))
         clock3 = Clock(hour=1)
-        scene.remove(clock)
-        scene.play(Transform(clock2, clock3))
+
+
+        scene.play(ReplacementTransform(clock2, clock3))
         scene.play(FadeOut(angle2))
 
-        scene.remove(clock2)
-        self.new_subsection(scene, "Add 2 modulo")
-        clock4 = Clock(use_zero=True, hour=1)
-        scene.play(Transform(clock3, clock4))
-
         self.new_subsection(scene, "Use zero")
-        scene.remove(clock3)
-        clock5 = Clock(use_zero=True, hour=4, modulus=41)
-        scene.play(Transform(clock4, clock5))
+        clock4 = Clock(use_zero=True, hour=1)
+        scene.play(ReplacementTransform(clock3, clock4))
+
+        self.new_subsection(scene, "Modulo 200")
+        clock5 = Clock(use_zero=True, hour=4, modulus=200)
+        scene.play(ReplacementTransform(clock4, clock5))
+        scene.wait(1)
 
         self.new_subsection(scene, "Modulo 41")
-        scene.remove(clock4)
-        scene.play(clock5.animate.align_on_border(LEFT, buff=0.2))
+        clock6 = Clock(use_zero=True, hour=4, modulus=41)
+        scene.play(ReplacementTransform(clock5, clock6))
+
+        self.new_subsection(scene, "Sidebar")
+        scene.play(clock6.animate.align_on_border(LEFT, buff=0.2))
         self.sidebar = Sidebar("Prime Field", code_path="data/pf/field.py")
         self.sidebar.animate_in(scene)
+
+
