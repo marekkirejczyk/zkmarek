73c6b237a6f14953ecd0f0ffb55e710f7f58c297
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-08-07 15:19:27 +0200

    Slide 6 done

--- zkmarek/video/slides/cec/operations.py
+++ zkmarek/video/slides/cec/operations.py
@@ -1,12 +1,13 @@
-from manim import LEFT, FadeOut, ReplacementTransform, Scene, VGroup, Write
+from manim import (LEFT, FadeOut, Indicate, ReplacementTransform, Scene,
+                   VGroup, Write)
 
 from zkmarek.crypto.cec_affine import CECAffine
+from zkmarek.video.constant import HIGHLIGHT_COLOR
 from zkmarek.video.mobjects.continuous_elliptic_chart import \
     ContinuousEllipticChart
 from zkmarek.video.mobjects.dot_on_curve import DotOnCurve
 from zkmarek.video.mobjects.sidebar import Sidebar
 from zkmarek.video.slides.common.slide_base import SlideBase
-from zkmarek.video.constant import HIGHLIGHT_COLOR
 
 
 class Operations(SlideBase):
@@ -28,19 +29,26 @@ class Operations(SlideBase):
         self.play_sound(scene, "data/sound/episode/s6-1.wav")
         self.chart.animate_in(scene)
         self.p1.animate_in(scene)
+        scene.wait(0.5)
         scene.play(ReplacementTransform(self.p1, self.p2))
+
+        scene.wait(3)
+        scene.play(VGroup(self.p2, self.chart).animate.align_on_border(LEFT), run_time=2)
+        self.sidebar.animate_show_label(scene)
+        scene.wait(3)
+        self.sidebar.animate_show_math(scene)
+
         self.new_subsection(scene,
             "Operations",
             sound="data/sound/episode/s6-2.wav")
-        scene.play(VGroup(self.p2, self.chart).animate.align_on_border(LEFT))
-        scene.play(Write(self.sidebar))
-
+        scene.wait(3)
         list = self.sidebar.math
-        list.set_color_by_tex("Negation", HIGHLIGHT_COLOR)
+        list[0][0:12].set_color(HIGHLIGHT_COLOR)
         scene.wait(2)
-        list.set_color_by_tex("Addition", HIGHLIGHT_COLOR)
+        list[0][12:25].set_color(HIGHLIGHT_COLOR)
         scene.wait(2)
-        list.set_color_by_tex("Doubling", HIGHLIGHT_COLOR)
+        list[0][25:38].set_color(HIGHLIGHT_COLOR)
+
 
 
     def animate_out(self, scene: Scene):
