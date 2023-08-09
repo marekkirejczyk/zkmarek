a9c78fb4490d690ab666cbb602d7d10fe437bb74
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-08-09 12:13:45 +0200

    Slide 10: animations done

--- zkmarek/video/slides/prime_fields.py
+++ zkmarek/video/slides/prime_fields.py
@@ -1,8 +1,10 @@
 from typing import Optional
 
-from manim import (DOWN, LEFT, RED, FadeIn, FadeOut, NumberLine,
-                   ReplacementTransform, TransformMatchingShapes)
+from manim import (DOWN, LEFT, RED, FadeIn, FadeOut, Indicate, NumberLine,
+                   ReplacementTransform, MathTex, TransformMatchingShapes,
+                   TransformMatchingTex)
 
+from zkmarek.video.constant import PRIMARY_COLOR
 from zkmarek.video.mobjects.clock import Clock
 from zkmarek.video.mobjects.fill_angle import FillAngle
 from zkmarek.video.mobjects.sidebar import Sidebar
@@ -13,45 +15,84 @@ from .common.slide_base import SlideBase
 class PrimeFields(SlideBase):
     clock: Optional[Clock] = None
     sidebar: Sidebar
+    order12: MathTex
+    order256: MathTex
+    order41: MathTex
 
     def __init__(self):
         super().__init__(title="Prime Fields")
 
+
     def animate_in(self, scene):
+        self.new_subsection(scene,
+            "Prime fields - clock",
+            sound="data/sound/episode/s10-1.wav")
+
         clock = Clock(hour=9)
         clock.set_z_index(3)
         clock.animate_in(scene)
-        self.new_subsection(scene, "Add 2")
         angle = FillAngle(deg1=270, deg2=330)
-        scene.play(FadeIn(angle))
+        scene.wait(2)
 
+        scene.play(FadeIn(angle))
+        scene.wait(1)
         clock2 = Clock(hour=11)
         scene.play(ReplacementTransform(clock, clock2))
         scene.play(FadeOut(angle))
 
-        self.new_subsection(scene, "Add 2 modulo")
         angle2 = FillAngle(deg1=330, deg2=30)
         scene.play(FadeIn(angle2))
         clock3 = Clock(hour=1)
 
-
         scene.play(ReplacementTransform(clock2, clock3))
         scene.play(FadeOut(angle2))
 
-        self.new_subsection(scene, "Use zero")
-        clock4 = Clock(use_zero=True, hour=1)
-        scene.play(ReplacementTransform(clock3, clock4))
+        self.new_subsection(scene,
+            "Prime fields - use zero",
+            sound="data/sound/episode/s10-2.wav")
 
-        self.new_subsection(scene, "Modulo 200")
+        clock4 = Clock(use_zero=True, hour=1)
+        scene.wait(1)
+        scene.play(ReplacementTransform(clock3, clock4))
+        scene.wait(2)
+        scene.play(Indicate(clock4.markings[1]))
+        scene.wait(1)
+        scene.play(Indicate(clock4.markings[23]))
+
+        scene.wait(5)
+
+        self.order12 = MathTex("{{Order}} = 12", color=PRIMARY_COLOR).next_to(
+            clock.get_center(), direction=DOWN, buff=0.2)
+        scene.play(FadeIn(self.order12))
+
+        self.new_subsection(scene,
+            "Prime fields - modulo 200",
+            sound="data/sound/episode/s10-3.wav")
+
+        scene.wait(3)
         clock5 = Clock(use_zero=True, hour=4, modulus=200)
         scene.play(ReplacementTransform(clock4, clock5))
+
+        scene.wait(2)
+
+        self.order256 = MathTex("{{Order}} = 2^{256}", color=PRIMARY_COLOR).next_to(
+            clock.get_center(), direction=DOWN, buff=0.2)
+        scene.play(TransformMatchingTex(self.order12, self.order256))
+
+        self.new_subsection(scene,
+            "Prime fields - modulo 41",
+            sound="data/sound/episode/s10-4.wav")
         scene.wait(1)
-
-        self.new_subsection(scene, "Modulo 41")
         clock6 = Clock(use_zero=True, hour=4, modulus=41)
-        scene.play(ReplacementTransform(clock5, clock6))
 
-        self.new_subsection(scene, "Morph to line")
+        self.order41 = MathTex("{{Order}} = 41", color=PRIMARY_COLOR).next_to(
+            clock.get_center(), direction=DOWN, buff=0.2)
+        scene.play(ReplacementTransform(clock5, clock6),
+            TransformMatchingTex(self.order256, self.order41), run_time=2)
+
+        scene.wait(4)
+        scene.play(FadeOut(self.order41))
+
         line = NumberLine(
             x_range=[0, 41, 2],
             length=10,
