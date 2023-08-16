7536f4491a6b0eb0ae4e3030db428feb079ef422
Marek Kirejczyk <marekkirejczyk@users.noreply.github.com>
2023-08-16 21:13:57 +0200

    Slide 26: done (#90)

--- zkmarek/video/mobjects/equation_box.py
+++ zkmarek/video/mobjects/equation_box.py
@@ -1,4 +1,5 @@
 from manim import LEFT, MathTex, SurroundingRectangle, Text, VGroup
+from zkmarek.video.constant import PRIMARY_COLOR
 from zkmarek.video.utils import chunks
 
 
@@ -8,7 +9,8 @@ class EquationBoxWithIcons(VGroup):
     def __init__(self, *mobjects, **kwargs):
         super().__init__(*mobjects, **kwargs)
         self.arrange_in_grid(cols=2, cell_alignment=LEFT)
-        self.rectangle = SurroundingRectangle(self, buff=0.2, corner_radius=0.1)
+        self.rectangle = SurroundingRectangle(self, buff=0.2,
+            corner_radius=0.1, color=PRIMARY_COLOR)
         self.add(self.rectangle)
         self.rectangle.set_center(self.get_center())
         self.scale(0.65)
@@ -24,10 +26,11 @@ class EquationBoxWithIcons(VGroup):
 class EquationBox(VGroup):
     rectangle: SurroundingRectangle
 
-    def __init__(self, *equations):
-        super().__init__(*[MathTex(eq) for eq in equations])
+    def __init__(self, *eqs):
+        super().__init__(*[MathTex(eq, color=PRIMARY_COLOR) for eq in eqs])
         self.arrange_in_grid(cols=1, cell_alignment=LEFT)
-        self.rectangle = SurroundingRectangle(self, buff=0.2, corner_radius=0.1)
+        self.rectangle = SurroundingRectangle(self, buff=0.2,
+            corner_radius=0.1, color=PRIMARY_COLOR)
         self.add(self.rectangle)
         self.rectangle.set_center(self.get_center())
         self.scale(0.65)
