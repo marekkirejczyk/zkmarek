dd3cbbed985f431730dd91c643e155ceed1427c2
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-08-16 11:08:00 +0200

    Slide 24: done

--- zkmarek/video/slides/ec/standards.py
+++ zkmarek/video/slides/ec/standards.py
@@ -1,4 +1,7 @@
-from manim import DOWN, UP, FadeIn, FadeOut, Text, VGroup
+from manim import (DOWN, UP, FadeIn, FadeOut, Indicate, ReplacementTransform,
+                   Text, VGroup)
+
+from zkmarek.video.constant import PRIMARY_COLOR, PRIMARY_FONT
 from zkmarek.video.mobjects.standard import (Standard, curve25519_standard,
                                              secp256k1_standard)
 from zkmarek.video.slides.common.slide_base import SlideBase
@@ -18,16 +21,56 @@ class Standards(SlideBase):
     def construct(self):
         self.secp256k1 = secp256k1_standard()
         self.curve25519 = curve25519_standard()
-        self.label = Text(self.title)
+        self.label = Text(self.title, font=PRIMARY_FONT, color=PRIMARY_COLOR)
         self.label.to_edge(UP)
-        self.group = VGroup(self.secp256k1, self.curve25519).arrange_in_grid(cols=2)
+        self.group = VGroup(self.secp256k1, self.curve25519
+            ).arrange_in_grid(cols=2)
         self.group.next_to(self.label, DOWN)
 
     def animate_in(self, scene):
-        scene.play(FadeIn(self.label))
-        scene.play(FadeIn(self.secp256k1))
-        self.new_subsection(scene, "Curve25519")
+        self.new_subsection(scene, "Operations",
+            sound="data/sound/episode/s24-1.wav")
+
+        secp = []
+        for i in range(0, 4):
+            secp.append(self.secp256k1.copy_with_rows(i+1))
+        scene.play(FadeIn(self.label), FadeIn(secp[0]))
+        scene.wait(8)
+        scene.play(ReplacementTransform(secp[0], secp[2]))
+        scene.play(Indicate(secp[2].rows[0][0][1] ))
+        scene.play(Indicate(secp[2].rows[1][0]))
+        scene.play(Indicate(secp[2].rows[2][0]))
+        self.new_subsection(scene, "Operations",
+            sound="data/sound/episode/s24-2.wav")
+        scene.wait(1.5)
+        scene.play(Indicate(secp[2].rows[1][0][1]))
+
+        self.new_subsection(scene, "Operations",
+            sound="data/sound/episode/s24-3.wav")
+        scene.play(ReplacementTransform(secp[2], secp[3]))
+        scene.wait(2)
+        scene.play(Indicate(secp[3].rows[3][0]))
+        self.new_subsection(scene, "Operations",
+            sound="data/sound/episode/s24-4.wav")
+        scene.play(ReplacementTransform(secp[3], self.secp256k1))
+        scene.play(Indicate(self.secp256k1.rows[4][0]))
+        scene.wait(8)
+        scene.play(Indicate(self.secp256k1.rows[4][0][1]))
+        self.new_subsection(scene, "Curve25519",
+            sound="data/sound/episode/s24-5.wav")
+
+        scene.wait(5)
         scene.play(FadeIn(self.curve25519))
+        scene.wait(3)
+        scene.play(Indicate(self.curve25519.rows[0][0][1]))
+        scene.play(Indicate(self.curve25519.rows[1][0]))
+        scene.wait(1)
+        scene.play(Indicate(self.curve25519.rows[2][0]))
+        scene.wait(2)
+        scene.play(Indicate(self.curve25519.rows[4][0]))
+        self.new_subsection(scene, "Curve25519",
+            sound="data/sound/episode/s24-6.wav")
+        scene.wait(13)
 
     def animate_out(self, scene):
-        scene.play(FadeOut(self.secp256k1))
+        scene.play(FadeOut(self.secp256k1), FadeOut(self.curve25519))
