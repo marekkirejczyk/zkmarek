774f8739b8137049bb07f7023390b89d96309868
Arkadiusz Konior <akonior@gmail.com>
2023-09-11 11:49:42 +0200

    Introduce standard

---
+++ zkmarek/crypto/standard.py
@@ -0,0 +1,16 @@
+from zkmarek.crypto.ec_affine import ECAffine
+from zkmarek.crypto.weierstrass_curve import WeierstrassCurve, Secp256k1
+
+
+class Standard:
+    curve: WeierstrassCurve
+    generator: ECAffine
+
+    def __init__(self, curve: WeierstrassCurve, generator_x: int, generator_sgn: int):
+        self.curve = curve
+        self.generator = ECAffine.from_x(generator_x, generator_sgn, Secp256k1)
+
+
+Secp256 = Standard(Secp256k1,
+    0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798, 0)
+
---
+++ zkmarek/test/test_standard.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+from zkmarek.crypto.standard import Secp256
+from zkmarek.crypto.weierstrass_curve import Secp256k1
+
+
+class TestStandard(TestCase):
+    def test_init(self):
+        generator = Secp256.generator
+        self.assertEqual(Secp256.curve, Secp256k1)
+        self.assertEqual(
+            0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798,
+            generator.x.value
+        )
+        self.assertEqual(
+            0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8,
+            generator.y.value
+        )
