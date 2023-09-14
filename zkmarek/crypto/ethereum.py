3b15fbefa4bf6e615ad18f055cdf9007c2500114
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-09-14 11:56:13 +0200

    Linter =)

--- zkmarek/crypto/ethereum.py
+++ zkmarek/crypto/ethereum.py
@@ -1,6 +1,6 @@
 from Crypto.Hash import keccak
 
-MESSAGE_PREFIX: str = "\x19Ethereum Signed Message:\n";
+MESSAGE_PREFIX: str = "\x19Ethereum Signed Message:\n"
 
 def hash_message(msg: str) -> str:
     prefix = MESSAGE_PREFIX.encode('utf-8')
