516e24f91ab136884dc4986bce99629e19262f8b
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-09-14 11:53:28 +0200

    Introduce ethereum hash_message

---
+++ zkmarek/crypto/ethereum.py
@@ -0,0 +1,12 @@
+from Crypto.Hash import keccak
+
+MESSAGE_PREFIX: str = "\x19Ethereum Signed Message:\n";
+
+def hash_message(msg: str) -> str:
+    prefix = MESSAGE_PREFIX.encode('utf-8')
+    msg_len = str(len(msg)).encode('utf-8')
+    message = msg.encode('utf-8')
+    payload = b''.join([prefix, msg_len, message])
+    k = keccak.new(digest_bits=256)
+    k.update(payload)
+    return k.hexdigest()
---
+++ zkmarek/test/test_ethereum.py
@@ -0,0 +1,13 @@
+import unittest
+from zkmarek.crypto.ethereum import hash_message
+
+
+TEST_SECRET: str = "9de347a715a200cd8e83cecc4277c7fdf2ebd95766720abec8364d879483b69b"
+TEST_MESSAGE: str = "Whatever"
+
+class TestEthereum(unittest.TestCase):
+
+    def test_hash_message(self):
+            actual = hash_message(TEST_MESSAGE)
+            expected = "2de4a69a7663970b80a2e37678eb1f73b0fe9209bfd4a191fea3d31588e451fd"
+            self.assertEqual(expected.lower(), actual.lower())
