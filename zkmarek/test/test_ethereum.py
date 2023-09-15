58742becdaceb65bf89621664f5462936c73f25f
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-09-15 07:40:13 +0200

    Remove over intendion

--- zkmarek/test/test_ethereum.py
+++ zkmarek/test/test_ethereum.py
@@ -8,6 +8,6 @@ TEST_MESSAGE: str = "Whatever"
 class TestEthereum(unittest.TestCase):
 
     def test_hash_message(self):
-            actual = hash_message(TEST_MESSAGE)
-            expected = "2de4a69a7663970b80a2e37678eb1f73b0fe9209bfd4a191fea3d31588e451fd"
-            self.assertEqual(expected.lower(), actual.lower())
+        actual = hash_message(TEST_MESSAGE)
+        expected = "2de4a69a7663970b80a2e37678eb1f73b0fe9209bfd4a191fea3d31588e451fd"
+        self.assertEqual(expected.lower(), actual.lower())
