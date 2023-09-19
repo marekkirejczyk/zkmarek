cf1d4b2a0319b847dba25582cca004feb1162871
Arkadiusz Konior <akonior@gmail.com>
2023-09-19 09:20:32 +0200

    Extract tests against external library to separate file

---
+++ zkmarek/test/test_against_external_cryptography_library.py
@@ -0,0 +1,41 @@
+import hashlib
+import unittest
+
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric.utils import decode_dss_signature
+
+from zkmarek.crypto.ec_affine import ECAffine
+from zkmarek.crypto.standard import Secp256
+
+
+class TestAgainstExternalCryptographyLibrary(unittest.TestCase):
+
+    def test_sign_with_random_k_and_verify_by_cryptography_library(self):
+        standard = Secp256
+        for i in range(100):
+            sk: int = standard.generate_secret_key()
+            pk: ECAffine = standard.generate_public_key(sk)
+            message: bytes = b'abc'
+            sig: bytes = standard.sign_with_random_k(sk, message).to_der()
+
+            vk = ec.EllipticCurvePublicNumbers(pk.x.value, pk.y.value, curve=ec.SECP256K1()).public_key()
+            vk.verify(sig, message, ec.ECDSA(hashes.SHA256()))
+
+    def test_verify_with_signature_generated_by_cryptography_library(self):
+        for i in range(100):
+            private_key = ec.generate_private_key(ec.SECP256K1())
+            public_key = private_key.public_key()
+            msg = b'abc'
+            signature = private_key.sign(msg, ec.ECDSA(hashes.SHA256()))
+            r, s = decode_dss_signature(signature)
+
+            msg_hash = hashlib.sha256(msg).digest()
+            z = int.from_bytes(msg_hash, 'big')
+            standard = Secp256
+            result = standard.verify(z, r, s, ECAffine(
+                public_key.public_numbers().x,
+                public_key.public_numbers().y,
+                standard.curve
+            ))
+            self.assertTrue(result)
