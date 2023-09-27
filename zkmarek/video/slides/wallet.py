3af1305b8dd65c281abc806477fa15b1ae20c1fe
Arkadiusz Konior <akonior@gmail.com>
2023-09-27 10:37:45 +0200

    Generate random secret key animation

--- zkmarek/video/slides/wallet.py
+++ zkmarek/video/slides/wallet.py
@@ -1,3 +1,4 @@
+from manim import UP, MoveToTarget, RIGHT, Scene, ScaleInPlace
 
 from zkmarek.video.mobjects.wallet import Wallet
 from zkmarek.video.slides.common.slide_base import SlideBase
@@ -5,6 +6,8 @@ from zkmarek.video.slides.common.slide_base import SlideBase
 
 class WalletSlide(SlideBase):
     wallet: Wallet
+    scene: Scene
+
     def __init__(self):
         super().__init__("Ethereum Wallet")
 
@@ -13,3 +16,9 @@ class WalletSlide(SlideBase):
 
     def animate_in(self, scene):
         self.wallet.animate_in(scene)
+        self.wallet.generate_random_secret_key(scene, 7)
+        scene.wait()
+        scene.play(ScaleInPlace(self.wallet, 0.5))
+        self.wallet.generate_target()
+        self.wallet.target.shift(2 * UP + 4 * RIGHT)
+        scene.play(MoveToTarget(self.wallet))
