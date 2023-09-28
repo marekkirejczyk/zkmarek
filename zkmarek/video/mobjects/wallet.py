cf71290590a33ba091f8731c6178f9d2bbea25a8
Arkadiusz Konior <akonior@gmail.com>
2023-09-28 16:56:55 +0200

    Animate moving generated address to wallet picture (#132)
    * Animate moving generated address to wallet picture
    
    * remove public_key_coordinates default value

--- zkmarek/video/mobjects/wallet.py
+++ zkmarek/video/mobjects/wallet.py
@@ -1,7 +1,7 @@
 from random import randint
 
 from manim import (LEFT, ORIGIN, RIGHT, Circle, FadeIn, RoundedRectangle, Text,
-                   VGroup, Write, ReplacementTransform)
+                   VGroup, Write, ReplacementTransform, Scene, DOWN, Animation)
 
 from zkmarek.video.constant import (BACKGROUND_COLOR, HIGHLIGHT_COLOR,
                                     PRIMARY_COLOR, SECONDARY_COLOR, PRIMARY_FONT)
@@ -14,7 +14,9 @@ class Wallet(VGroup):
     secret_key: VGroup
     secret_key_label: Text
     secret_key_value: Text
-    address: Text
+    address: VGroup
+    address_label: Text
+    address_value: Text
 
     def __init__(self, secret_key="", address=""):
         super().__init__()
@@ -22,15 +24,18 @@ class Wallet(VGroup):
         self.secret_key_value = Text("", font_size=24, font=PRIMARY_FONT, color=SECONDARY_COLOR)
         self.secret_key_value.next_to(self.secret_key_label, RIGHT)
         self.secret_key = VGroup(self.secret_key_label, self.secret_key_value)
+        self.secret_key.shift(1.7 * LEFT)
 
-        self.address = Text(address, font_size=24, font=PRIMARY_FONT, color=HIGHLIGHT_COLOR)
-        self.secret_key.shift(0.3 * LEFT)
-        self.address.shift(0.3 * LEFT)
+        self.address_label = Text(address, font_size=24, font=PRIMARY_FONT, color=HIGHLIGHT_COLOR)
+        self.address_value = Text("", font_size=14, font=PRIMARY_FONT, color=HIGHLIGHT_COLOR)
+        self.address_value.next_to(self.address_label, RIGHT)
+        self.address = VGroup(self.address_label, self.address_value)
+        self.address.shift(1.7 * LEFT)
         self.add(self.secret_key, self.address)
         self.arrange_in_grid(cols=1, cell_alignment=LEFT)
 
-        width = self.get_width() + 3
-        height = self.get_height() + 1
+        width = self.get_width() + 4
+        height = self.get_height() + 2
         self.border = RoundedRectangle(width=width, height=height, corner_radius=0.3, color=PRIMARY_COLOR)
         self.border.move_to(RIGHT * 0.3)
         self.clip = RoundedRectangle(width=0.6, height=0.5, corner_radius=0.1, color=PRIMARY_COLOR,
@@ -40,15 +45,15 @@ class Wallet(VGroup):
         self.button.next_to(self.clip, ORIGIN)
         self.add(self.border, self.clip, self.button)
 
-    def animate_in(self, scene):
+    def animate_in(self, scene: Scene):
         scene.play(FadeIn(self.border), FadeIn(self.clip), FadeIn(self.button))
         scene.play(Write(self.secret_key))
         scene.play(Write(self.address))
 
-    def animate_random_secret_key(self, scene, final_value: int, rand_max: int):
+    def animate_random_secret_key(self, scene: Scene, final_value: int, rand_max: int):
         rounds = 20
         for key in [str(randint(0, rand_max)) for i in range(rounds)] + [str(final_value), hex(final_value)]:
-            new_secret_key_value: Text = Text(f"= {key}", font_size=24, font=PRIMARY_FONT, color=SECONDARY_COLOR)
+            new_secret_key_value: Text = Text(f"{key}", font_size=24, font=PRIMARY_FONT, color=SECONDARY_COLOR)
             new_secret_key_value.next_to(self.secret_key_label, RIGHT)
             last = key.startswith('0x')
             if last:
@@ -58,3 +63,10 @@ class Wallet(VGroup):
                 run_time=1 if last else 0.1
             )
             self.secret_key_value = new_secret_key_value
+
+    def animate_address_value(self, scene: Scene, address_value: str, with_animation: Animation):
+        new_address_value = Text(address_value, font_size=14, font=PRIMARY_FONT, color=HIGHLIGHT_COLOR)
+        new_address_value.next_to(self.address_label, RIGHT)
+        new_address_value.shift(0.05 * DOWN)
+        scene.play(ReplacementTransform(self.address_value, new_address_value), with_animation)
+        self.address_value = new_address_value
