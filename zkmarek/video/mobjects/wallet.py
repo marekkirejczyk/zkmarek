78445d5534cd93a2f2be2515694bbf8b4330c40a
Arkadiusz Konior <akonior@gmail.com>
2023-09-29 11:52:18 +0200

    Update wallet with big numbers (#133)

--- zkmarek/video/mobjects/wallet.py
+++ zkmarek/video/mobjects/wallet.py
@@ -34,7 +34,7 @@ class Wallet(VGroup):
         self.add(self.secret_key, self.address)
         self.arrange_in_grid(cols=1, cell_alignment=LEFT)
 
-        width = self.get_width() + 4
+        width = self.get_width() + 5
         height = self.get_height() + 2
         self.border = RoundedRectangle(width=width, height=height, corner_radius=0.3, color=PRIMARY_COLOR)
         self.border.move_to(RIGHT * 0.3)
@@ -53,20 +53,23 @@ class Wallet(VGroup):
     def animate_random_secret_key(self, scene: Scene, final_value: int, rand_max: int):
         rounds = 20
         for key in [str(randint(0, rand_max)) for i in range(rounds)] + [str(final_value), hex(final_value)]:
-            new_secret_key_value: Text = Text(f"{key}", font_size=24, font=PRIMARY_FONT, color=SECONDARY_COLOR)
-            new_secret_key_value.next_to(self.secret_key_label, RIGHT)
             last = key.startswith('0x')
             if last:
                 scene.wait()
-            scene.play(
-                ReplacementTransform(self.secret_key_value, new_secret_key_value),
-                run_time=1 if last else 0.1
-            )
-            self.secret_key_value = new_secret_key_value
+            self.animate_private_key(scene, key, 1 if last else 0.1)
 
-    def animate_address_value(self, scene: Scene, address_value: str, with_animation: Animation):
+    def animate_private_key(self, scene, key, run_time: float = 1, font_size=24):
+        new_secret_key_value: Text = Text(f"{key}", font_size=font_size, font=PRIMARY_FONT, color=SECONDARY_COLOR)
+        new_secret_key_value.next_to(self.secret_key_label, RIGHT)
+        scene.play(
+            ReplacementTransform(self.secret_key_value, new_secret_key_value),
+            run_time=run_time
+        )
+        self.secret_key_value = new_secret_key_value
+
+    def animate_address_value(self, scene: Scene, address_value: str, *with_animations: Animation):
         new_address_value = Text(address_value, font_size=14, font=PRIMARY_FONT, color=HIGHLIGHT_COLOR)
         new_address_value.next_to(self.address_label, RIGHT)
         new_address_value.shift(0.05 * DOWN)
-        scene.play(ReplacementTransform(self.address_value, new_address_value), with_animation)
+        scene.play(ReplacementTransform(self.address_value, new_address_value), *with_animations)
         self.address_value = new_address_value
