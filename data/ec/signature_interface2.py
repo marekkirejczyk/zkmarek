eb18b897012344a8f01ad69c7f5ece26fce383fe
Arkadiusz Konior <akonior@gmail.com>
2023-12-12 12:37:38 +0100

    verify method and animations

--- data/ec/signature_interface2.py
+++ data/ec/signature_interface2.py
@@ -6,3 +6,6 @@ def sing(message, secret_key) -> signature
 
 
 def verify(message, signature, public_key) -> bool
+
+
+def recover(message, signature) -> public_key
--- zkmarek/video/slides/ec/digital_signature_interface.py
+++ zkmarek/video/slides/ec/digital_signature_interface.py
@@ -1,3 +1,5 @@
+from manim import ReplacementTransform, Code, DOWN
+
 from zkmarek.video.slides.common.code_slide import CodeSlide
 
 
@@ -9,4 +11,29 @@ class DigitalSignatureInterface(CodeSlide):
             font_size=24,
             background='rectangle',
             insert_line_no=False
+        )
+
+    def animate_in(self, scene):
+        super().animate_in(scene)
+        self.replace_code(scene, "data/ec/signature_interface2.py")
+        scene.wait(2)
+        self.replace_code(scene, "data/ec/signature_interface3.py")
+
+    def replace_code(self, scene, path: str):
+        code = self._get_code(path)
+        code.next_to(self.title_text, DOWN, buff=0.5)
+        scene.play(ReplacementTransform(self.code, code))
+        self.code = code
+
+    @staticmethod
+    def _get_code(path: str):
+        return Code(
+            path,
+            font_size=24,
+            background='rectangle',
+            insert_line_no=False,
+            font="Monospace",
+            margin=0.2,
+            style="fruity",
+            line_no_buff=0.2,
         )
