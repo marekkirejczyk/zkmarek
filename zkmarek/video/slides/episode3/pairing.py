fb51a1592954cf0052b718fcd443d3fec01748ee
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2024-03-18 18:00:19 +0100

    Add info about pairing group types

--- zkmarek/video/slides/episode3/pairing.py
+++ zkmarek/video/slides/episode3/pairing.py
@@ -1,10 +1,19 @@
-from manim import MathTex, Write, Unwrite, DOWN, UP
+from manim import (DOWN, UP, Brace, FadeIn, FadeOut, MathTex, Text, Unwrite,
+                   Write)
+
 from zkmarek.video.constant import PRIMARY_COLOR, SECONDARY_COLOR
 from zkmarek.video.slides.common.slide_base import SlideBase
+
 
 class Pairing(SlideBase):
     definition_label: MathTex
     definition: MathTex
+    brace1: Brace
+    brace2: Brace
+    brace3: Brace
+    brace1_label: Text
+    brace2_label: Text
+    brace3_label: Text
     bilinearity_label: MathTex
     bilinearity: MathTex
     bilinearity_ext: MathTex
@@ -16,10 +25,23 @@ class Pairing(SlideBase):
 
     def construct(self):
         self.definition_label = MathTex(r"Pairing", font_size=50, color=PRIMARY_COLOR)
-        self.definition = MathTex(r"e: G_1  \times G_2 \rightarrow G_T", font_size=70, color=PRIMARY_COLOR)
+        self.definition = MathTex(r"e: {{G_1}}  \times {{G_2}} \rightarrow {{G_T}}", font_size=70, color=PRIMARY_COLOR)
+        self.brace1 = Brace(self.definition[1], DOWN, color=PRIMARY_COLOR)
+        self.brace2 = Brace(self.definition[3], DOWN, color=PRIMARY_COLOR)
+        self.brace3 = Brace(self.definition[5], DOWN, color=PRIMARY_COLOR)
+        self.brace1.shift(UP * 2.5)
+        self.brace2.shift(UP * 2.5)
+        self.brace3.shift(UP * 2.5)
+        self.brace1_label = Text(r"Subgroup of points on elliptic curve", font_size=30, color=PRIMARY_COLOR)
+        self.brace1.put_at_tip(self.brace1_label)
+        self.brace2_label = Text(r"Subgroup of points on elliptic curve", font_size=30, color=PRIMARY_COLOR)
+        self.brace2.put_at_tip(self.brace2_label)
+        self.brace3_label = Text(r"Prime field", font_size=30, color=PRIMARY_COLOR)
+        self.brace3.put_at_tip(self.brace3_label)
+
         self.bilinearity_label = MathTex(r"Bilinearity", font_size=40, color=SECONDARY_COLOR)
         self.bilinearity = MathTex(r"e(P + P', Q) = e(P, Q) \cdot e(P', Q)", font_size=40, color=SECONDARY_COLOR)
-        bilinearity_tex = r"e([a]P, [b]Q) = e(P, [b]Q)^a = e([a]P, Q)^b = e(P, Q)^{ab} = e([b]P, [a]Q)"
+        bilinearity_tex = r"e(aP, bQ) = e(P, bQ)^a = e(aP, Q)^b = e(P, Q)^{ab} = e(bP, aQ)"
         self.bilinearity_ext = MathTex(bilinearity_tex, font_size=40, color=SECONDARY_COLOR)
         self.non_degeneracy_label = MathTex(r"Non-degeneracy", font_size=40, color=SECONDARY_COLOR)
         nd_text = r"\forall{a \in G_1}, \forall{b \in G_2} (a, b \neq \mathcal{O}"
@@ -39,10 +61,25 @@ class Pairing(SlideBase):
     def animate_in(self, scene):
         scene.play(Write(self.definition_label))
         scene.play(Write(self.definition))
+
+        scene.play(FadeIn(self.brace1))
+        scene.play(FadeIn(self.brace1_label))
+        scene.play(FadeOut(self.brace1), FadeOut(self.brace1_label))
+
+        scene.play(FadeIn(self.brace2))
+        scene.play(FadeIn(self.brace2_label))
+        scene.play(FadeOut(self.brace2), FadeOut(self.brace2_label))
+
+        scene.play(FadeIn(self.brace3))
+        scene.play(FadeIn(self.brace3_label))
+        scene.play(FadeOut(self.brace3), FadeOut(self.brace3_label))
+
         scene.play(Write(self.bilinearity_label))
         scene.play(Write(self.bilinearity))
         scene.play(Write(self.bilinearity_ext))
-        scene.play(Unwrite(self.bilinearity_ext))
+        scene.play(Unwrite(self.bilinearity))
+        scene.play(self.bilinearity_ext.animate.next_to(self.bilinearity_label, DOWN))
+
         scene.play(Write(self.non_degeneracy_label))
         scene.play(Write(self.non_degeneracy))
         scene.play(Write(self.computability_label))
