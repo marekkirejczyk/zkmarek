f400ea486ebd6d8c4ef9f60e13f81b4cd91236a1
marta <wleklinskam@gmail.com>
2024-05-01 15:52:15 +0200

    fixed test

--- zkmarek/video/slides/episode2/PreviouslyOn.py
+++ zkmarek/video/slides/episode2/PreviouslyOn.py
@@ -15,15 +15,11 @@ from manim import (
     ReplacementTransform,
     SurroundingRectangle,
     Transform,
-    TransformMatchingTex,
-    Group,
     MoveToTarget,
-    DEFAULT_FONT_SIZE,
-    ScaleInPlace,
 )
 from typing import List
 
-from zkmarek.crypto.weierstrass_curve import WeierstrassCurve, Secp256k1_41
+from zkmarek.crypto.weierstrass_curve import Secp256k1_41
 from zkmarek.video.constant import (
     PRIMARY_COLOR,
     BACKGROUND_COLOR,
@@ -88,53 +84,53 @@ class PreviouslyOn(SlideBase):
         scene.wait(2)
 
         # positions - weierstrass eqn
-        # self.weierstrass_form.next_to(self.weierstrass_equation, UP)
+        self.weierstrass_form.next_to(self.weierstrass_equation, UP)
 
-        # # animating weierstrass eqn
-        # self.new_subsection(
-        #     scene, "equation", sound="data/sound/episode2/ec_prev_on1.mp3"
-        # )
-        # self.chart.animate_align_left(scene)
-        # scene.play(Write(self.weierstrass_form.to_edge(RIGHT)))
-        # scene.wait(3)
-        # scene.play(Write(self.weierstrass_equation.to_edge(RIGHT)))
-        # # scene.play(Write(self.weierstrass_equation), run_time=2)
-        # scene.wait(2)
-        # scene.play(FadeOut(self.weierstrass_form), FadeOut(self.weierstrass_equation))
+        # animating weierstrass eqn
+        self.new_subsection(
+            scene, "equation", sound="data/sound/episode2/ec_prev_on1.mp3"
+        )
+        self.chart.animate_align_left(scene)
+        scene.play(Write(self.weierstrass_form.to_edge(RIGHT)))
+        scene.wait(3)
+        scene.play(Write(self.weierstrass_equation.to_edge(RIGHT)))
+        # scene.play(Write(self.weierstrass_equation), run_time=2)
+        scene.wait(2)
+        scene.play(FadeOut(self.weierstrass_form), FadeOut(self.weierstrass_equation))
 
-        # # positions operations
-        # self.sidebar2.next_to(self.sidebar, DOWN * 1)
-        # self.sidebar3.next_to(self.sidebar2, DOWN * 1)
-        # self.sidebar4.next_to(self.sidebar3, DOWN * 1)
+        # positions operations
+        self.sidebar2.next_to(self.sidebar, DOWN * 1)
+        self.sidebar3.next_to(self.sidebar2, DOWN * 1)
+        self.sidebar4.next_to(self.sidebar3, DOWN * 1)
 
-        # # animating operations on eliptic curves
-        # self.new_subsection(
-        #     scene, "Operations", sound="data/sound/episode2/ec_prev_on2.mp3"
-        # )
-        # scene.play(FadeIn(self.sidebar), run_time=0.5)
-        # scene.play(FadeIn(self.sidebar2), run_time=0.5)
-        # scene.play(FadeIn(self.sidebar3), run_time=0.5)
-        # # adjust time to what is showing and do highlighted text when mentioned (negating, adding and multiplying)
-        # scene.wait(2)
-        # framebox1 = SurroundingRectangle(self.sidebar, buff=0.1)
-        # framebox2 = SurroundingRectangle(self.sidebar2, buff=0.1)
-        # framebox3 = SurroundingRectangle(self.sidebar3, buff=0.1)
-        # framebox4 = SurroundingRectangle(self.sidebar4, buff=0.1)
-        # scene.play(ReplacementTransform(framebox1, framebox2), run_time=3)
-        # scene.play(ReplacementTransform(framebox2, framebox3), run_time=3)
+        # animating operations on eliptic curves
+        self.new_subsection(
+            scene, "Operations", sound="data/sound/episode2/ec_prev_on2.mp3"
+        )
+        scene.play(FadeIn(self.sidebar), run_time=0.5)
+        scene.play(FadeIn(self.sidebar2), run_time=0.5)
+        scene.play(FadeIn(self.sidebar3), run_time=0.5)
+        # adjust time to what is showing and do highlighted text when mentioned (negating, adding and multiplying)
+        scene.wait(2)
+        framebox1 = SurroundingRectangle(self.sidebar, buff=0.1)
+        framebox2 = SurroundingRectangle(self.sidebar2, buff=0.1)
+        framebox3 = SurroundingRectangle(self.sidebar3, buff=0.1)
+        framebox4 = SurroundingRectangle(self.sidebar4, buff=0.1)
+        scene.play(ReplacementTransform(framebox1, framebox2), run_time=3)
+        scene.play(ReplacementTransform(framebox2, framebox3), run_time=3)
 
-        # self.new_subsection(
-        #     scene, "inverse", sound="data/sound/episode2/ec_prev_on3.mp3"
-        # )
-        # scene.play(Write(self.sidebar4), run_time=1)
-        # scene.play(ReplacementTransform(framebox3, framebox4), run_time=2)
-        # # animating out
-        # scene.play(FadeOut(self.sidebar), run_time=0.5)
-        # scene.play(FadeOut(self.sidebar2), run_time=0.5)
-        # scene.play(FadeOut(self.sidebar3), run_time=0.5)
-        # scene.play(FadeOut(self.sidebar4), run_time=0.5)
-        # scene.play(FadeOut(framebox4), run_time=0.5)
-        # scene.play(FadeOut(self.chart))
+        self.new_subsection(
+            scene, "inverse", sound="data/sound/episode2/ec_prev_on3.mp3"
+        )
+        scene.play(Write(self.sidebar4), run_time=1)
+        scene.play(ReplacementTransform(framebox3, framebox4), run_time=2)
+        # animating out
+        scene.play(FadeOut(self.sidebar), run_time=0.5)
+        scene.play(FadeOut(self.sidebar2), run_time=0.5)
+        scene.play(FadeOut(self.sidebar3), run_time=0.5)
+        scene.play(FadeOut(self.sidebar4), run_time=0.5)
+        scene.play(FadeOut(framebox4), run_time=0.5)
+        scene.play(FadeOut(self.chart))
 
         # wallet
         self.animate_secret_key(scene)
