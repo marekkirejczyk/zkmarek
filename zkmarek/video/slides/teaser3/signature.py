2b184237f2539090bf64c764550ec989e4ff88c3
marta <wleklinskam@gmail.com>
2024-06-03 12:47:21 +0200

    proposition for slide 1 of T3 - added sound and animation

--- zkmarek/video/slides/teaser3/signature.py
+++ zkmarek/video/slides/teaser3/signature.py
@@ -4,15 +4,22 @@ from manim import (
     MathTex,
     UP,
     DOWN,
-    ReplacementTransform,
-    Text,
     TransformMatchingTex,
-    VGroup,
+    Text,
+    Indicate,
+    Write,
+    Unwrite,
 )
 
 from zkmarek.video.slides.common.slide_base import SlideBase
+from zkmarek.video.mobjects.strike_line import StrikeLine
 from zkmarek.video.mobjects.equation_box import EquationBox
-from zkmarek.video.constant import PRIMARY_COLOR, PRIMARY_FONT, SECONDARY_COLOR
+from zkmarek.video.constant import (
+    PRIMARY_COLOR,
+    PRIMARY_FONT,
+    SECONDARY_COLOR,
+    HIGHLIGHT_COLOR,
+)
 
 
 class SignatureMath(SlideBase):
@@ -21,7 +28,7 @@ class SignatureMath(SlideBase):
 
     def construct(self):
         self.title = Text(
-            "Previously on zkMarek", font=PRIMARY_FONT, color=PRIMARY_COLOR
+            "Previously on ZK Marek", font=PRIMARY_FONT, color=PRIMARY_COLOR
         )
 
         self.signature_box = EquationBox(
@@ -38,35 +45,19 @@ class SignatureMath(SlideBase):
             color=SECONDARY_COLOR,
         ).next_to(self.signature_box, UP * 2)
         self.equation5 = MathTex(
-            "Q = (-msg \cdot r^{-1} + {{ (msg + r \cdot K_{Priv}) \cdot secret^{-1} }} {{\cdot r^{-1} secret) \cdot  G}}",  # noqa: E501 # pyright: ignore
+            "Q = (-msg \cdot r^{-1} +  (msg + r \cdot K_{Priv}) \cdot {{secret^{-1} }} \cdot r^{-1} {{secret}}) \cdot  G}}",  # noqa: E501 # pyright: ignore
             color=SECONDARY_COLOR,
         ).next_to(self.signature_box, UP * 2)
-        self.equation6 = MathTex(
-            "Q = (-msg \cdot r^{-1} + (msg + r \cdot K_{\mathrm{Priv}}) {{\cdot secret^{-1}}}  \cdot r^{-1} {{secret}}) \cdot  G}}",  # noqa: E501 # pyright: ignore
-            color=SECONDARY_COLOR,
-        ).next_to(self.signature_box, UP * 2)
-        self.equation7 = MathTex(
-            "Q = (-msg \cdot r^{-1} + (msg + r \cdot K_{Priv}) {{}}  \cdot r^{-1} {{}}) \cdot G}}",  # noqa: E501 # pyright: ignore
-            color=SECONDARY_COLOR,
-        ).next_to(self.signature_box, UP * 2)
+        self.strike1 = StrikeLine(self.equation5[1])
+        self.strike2 = StrikeLine(self.equation5[3])
         self.equation8 = MathTex(
-            "Q = (-msg \cdot r^{-1} + ({{msg}} {{ }} + {{r \cdot}} K_{Priv}) {{\cdot r^{-1})}} \cdot G}}",  # noqa: E501 # pyright: ignore
+            "Q = (-msg \cdot r^{-1} + ({{msg}}  + {{r \cdot}} K_{Priv}) {{\cdot r^{-1})}} \cdot G}}",  # noqa: E501 # pyright: ignore
             color=SECONDARY_COLOR,
         ).next_to(self.signature_box, UP * 2)
-        self.equation9 = MathTex(
-            "Q = (-msg \cdot r^{-1} + ({{msg}} {{\cdot r^{-1} }} + {{ }} K_{Priv}) ) {{ }} \cdot G}}",  # noqa: E501 # pyright: ignore
-            color=SECONDARY_COLOR,
-        ).next_to(self.signature_box, UP * 2)
+        self.strike3 = StrikeLine(self.equation8[3])
+        self.strike4 = StrikeLine(self.equation8[5])
         self.equation10 = MathTex(
-            "Q = (-msg \cdot r^{-1} + (msg \cdot r^{-1} + K_{Priv}) ) \cdot G}}",
-            color=SECONDARY_COLOR,
-        ).next_to(self.signature_box, UP * 2)
-        self.equation11 = MathTex(
-            "Q = {{(-msg \cdot r^{-1} + }} {{(}}{{msg \cdot r^{-1} + }} K_{Priv} {{)}} {{ ) }} \cdot G}}",  # noqa: E501 # pyright: ignore
-            color=SECONDARY_COLOR,
-        ).next_to(self.signature_box, UP * 2)
-        self.equation12 = MathTex(
-            "Q = {{(-msg \cdot r^{-1} + }} {{ }}{{msg \cdot r^{-1} + }}  K_{Priv} {{ }} {{ ) }} \cdot G}}",  # noqa: E501 # pyright: ignore
+            "Q = (-msg \cdot r^{-1} + msg \cdot r^{-1} + K_{Priv}) \cdot G}}",
             color=SECONDARY_COLOR,
         ).next_to(self.signature_box, UP * 2)
         self.equation13 = MathTex(
@@ -75,39 +66,50 @@ class SignatureMath(SlideBase):
         ).next_to(self.signature_box, UP * 2)
 
     def animate_in(self, scene):
+        self.new_subsection(scene, "Intro", "data/sound/teaser3/slide1-0.mp3")
         scene.play(FadeIn(self.title.to_edge(UP)))
         scene.play(FadeIn(self.equation2), FadeIn(self.signature_box))
-        scene.wait(1)
+        scene.wait(1.2)
+        self.new_subsection(scene, "canceling out", "data/sound/teaser3/slide1-1.mp3")
         scene.play(
-            TransformMatchingTex(
-                VGroup(self.equation2, self.signature_box[1].copy()), self.equation4
-            ),
-            TransformMatchingTex(
-                VGroup(self.equation2, self.signature_box[2].copy()), self.equation4
-            ),
+            Indicate(self.signature_box[1], color=HIGHLIGHT_COLOR),
+            Indicate(self.signature_box[2], color=PRIMARY_COLOR),
+            Indicate(self.equation2[1], color=HIGHLIGHT_COLOR),
+            Indicate(self.equation2[3], color=PRIMARY_COLOR),
+        )
+        scene.wait(0.1)
+        scene.play(
+            TransformMatchingTex(self.equation2, self.equation4),
         )
         scene.wait(1)
         scene.play(
-            TransformMatchingTex(
-                VGroup(self.equation4, self.signature_box[0].copy()), self.equation5
-            ),
+            Indicate(self.signature_box[0], color=HIGHLIGHT_COLOR),
+            Indicate(self.equation4[1], color=HIGHLIGHT_COLOR),
         )
-        scene.wait(2)
-        scene.play(ReplacementTransform(self.equation5, self.equation6), run_time=0.5)
-        scene.wait(2)
-        scene.play(ReplacementTransform(self.equation6, self.equation7), run_time=0.5)
-        scene.wait(2)
-        scene.play(ReplacementTransform(self.equation7, self.equation8), run_time=0.5)
-        scene.wait(2)
-        scene.play(ReplacementTransform(self.equation8, self.equation9), run_time=0.5)
-        scene.wait(2)
-        scene.play(ReplacementTransform(self.equation9, self.equation10), run_time=0.5)
-        scene.wait(2)
-        scene.play(ReplacementTransform(self.equation10, self.equation11), run_time=0.5)
-        scene.wait(2)
-        scene.play(ReplacementTransform(self.equation11, self.equation12), run_time=0.5)
-        scene.wait(2)
-        scene.play(ReplacementTransform(self.equation12, self.equation13), run_time=0.5)
+        scene.wait(0.1)
+        scene.play(
+            TransformMatchingTex(self.equation4, self.equation5),
+        )
+        self.new_subsection(scene, "intuition", "data/sound/teaser3/slide1-2.mp3")
+        scene.wait(1)
+        scene.play(Write(self.strike1), Write(self.strike2))
+        scene.wait(0.1)
+        scene.play(
+            Unwrite(self.strike1),
+            Unwrite(self.strike2),
+            TransformMatchingTex(self.equation5, self.equation8),
+            run_time=0.5,
+        )
+        scene.wait(1)
+        scene.play(Write(self.strike3), Write(self.strike4))
+        scene.play(
+            Unwrite(self.strike3),
+            Unwrite(self.strike4),
+            TransformMatchingTex(self.equation8, self.equation10),
+            run_time=0.5,
+        )
+        scene.wait(1)
+        scene.play(TransformMatchingTex(self.equation10, self.equation13), run_time=0.5)
 
     def animate_out(self, scene):
         scene.play(
