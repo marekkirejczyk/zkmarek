66f21420072f0cf2b6247920a86268c7cfdcf911
martawleklinska <wleklinskam@gmail.com>
2024-08-18 07:38:26 +0200

    wrong point value

--- zkmarek/video/slides/e4/polynomials.py
+++ zkmarek/video/slides/e4/polynomials.py
@@ -67,7 +67,7 @@ class Polynomials(SlideBase):
         self.poly_point3 = MathTex("P(3) = 15", color = PRIMARY_COLOR).next_to(self.poly_point2, DOWN)
 
         self.p2 = ValueTracker(-2)
-        a2 = Curve.from_x(self.p25.get_value())
+        a2 = Curve.from_x(self.p2.get_value())
         self.root1 = DotOnCurve(self.chart.ax, "({{-2}},{{0}})", a2)
         self.p05 = ValueTracker(1.5)
         a05 = Curve.from_x(self.p05.get_value())
