afca5daa1d0c8ff5112a7fdcbe3a09bc07922e02
martawleklinska <wleklinskam@gmail.com>
2025-03-20 22:07:44 +0100

    changed code.code into code_lines in utils

--- zkmarek/video/utils.py
+++ zkmarek/video/utils.py
@@ -45,7 +45,6 @@ def get_deck_name(default_deck):
 #       original_text: str
 
 def find_in_code(code, subject):
-    #  code.code = remove_invisible_chars(code.code)
-     text = code.code.lines_text
+     text = code.code_lines
      arr = text._find_indexes(subject, text.original_text)
      return [text.chars[start:end] for start, end in arr]
