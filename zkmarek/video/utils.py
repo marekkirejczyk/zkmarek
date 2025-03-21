910df5b64fe019859bacf9e295a03ce8389757e8
martawleklinska <wleklinskam@gmail.com>
2025-03-21 09:04:42 +0100

    changed code_srting into code_lines.lines_tex

--- zkmarek/video/utils.py
+++ zkmarek/video/utils.py
@@ -45,6 +45,6 @@ def get_deck_name(default_deck):
 #       original_text: str
 
 def find_in_code(code, subject):
-     text = code.code_string
+     text = code.code_lines.lines_text
      arr = text._find_indexes(subject, text.original_text)
      return [text.chars[start:end] for start, end in arr]
