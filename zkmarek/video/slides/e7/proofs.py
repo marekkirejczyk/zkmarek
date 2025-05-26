bcd245c3b4a12fa00ad88d09c5c40f5fb04bbdeb
martawleklinska <wleklinskam@gmail.com>
2025-05-26 12:18:30 +0200

    changes to correct sound names in proof slide

--- zkmarek/video/slides/e7/proofs.py
+++ zkmarek/video/slides/e7/proofs.py
@@ -12,32 +12,32 @@ class Proofs(SlideBase):
         
     
     def animate_in(self, scene):
-        self.new_subsection(scene, "85: proof C, opening", "data/sound/e7/slide4-1.mp3")
+        self.new_subsection(scene, "85: proof C, opening", "data/sound/e7/slide5-1.mp3")
         scene.play(Write(self.title_label), run_time=0.7)
         
         
-        self.new_subsection(scene, "multi-level", "data/sound/e7/slide4-2.mp3")
+        self.new_subsection(scene, "multi-level", "data/sound/e7/slide5-2.mp3")
         
-        self.new_subsection(scene, "C0, opening, proof pi", "data/sound/e7/slide4-3.mp3")
+        self.new_subsection(scene, "C0, opening, proof pi", "data/sound/e7/slide5-3.mp3")
         
-        self.new_subsection(scene, "parent commitment, opening", "data/sound/e7/slide4-4.mp3")
+        self.new_subsection(scene, "parent commitment, opening", "data/sound/e7/slide5-4.mp3")
         
-        self.new_subsection(scene, "root, proof, opening", "data/sound/e7/slide4-5.mp3")
+        self.new_subsection(scene, "root, proof, opening", "data/sound/e7/slide5-5.mp3")
         
-        self.new_subsection(scene, "3 commitments, 3 openings, 3 proofs", "data/sound/e7/slide4-6.mp3")
+        self.new_subsection(scene, "3 commitments, 3 openings, 3 proofs", "data/sound/e7/slide5-6.mp3")
         
-        self.new_subsection(scene, "different weight", "data/sound/e7/slide4-6a.mp3")
+        self.new_subsection(scene, "different weight", "data/sound/e7/slide5-6a.mp3")
         
-        self.new_subsection(scene, "IPA", "data/sound/e7/slide4-6b.mp3")
+        self.new_subsection(scene, "IPA", "data/sound/e7/slide5-6b.mp3")
         
-        self.new_subsection(scene, "total 1728", "data/sound/e7/slide4-6c.mp3")
+        self.new_subsection(scene, "total 1728", "data/sound/e7/slide5-6c.mp3")
         
-        self.new_subsection(scene, "KZG", "data/sound/e7/slide4-6d.mp3")
+        self.new_subsection(scene, "KZG", "data/sound/e7/slide5-6d.mp3")
         
-        self.new_subsection(scene, "total 384 b", "data/sound/e7/slide4-6e.mp3")
+        self.new_subsection(scene, "total 384 b", "data/sound/e7/slide5-6e.mp3")
         
-        self.new_subsection(scene, "much smaller but pairings", "data/sound/e7/slide4-6f.mp3")
+        self.new_subsection(scene, "much smaller but pairings", "data/sound/e7/slide5-6f.mp3")
         
-        self.new_subsection(scene, "3 proofs - to verify", "data/sound/e7/slide4-6g.mp3")
+        self.new_subsection(scene, "3 proofs - to verify", "data/sound/e7/slide5-6g.mp3")
         
-        self.new_subsection(scene, "simplified to sinlge proof", "data/sound/e7/slide4-7.mp3")
+        self.new_subsection(scene, "simplified to sinlge proof", "data/sound/e7/slide5-7.mp3")
