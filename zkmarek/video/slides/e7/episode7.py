e674a35dfa9d1be344edbb971f34c582db0ec771
martawleklinska <wleklinskam@gmail.com>
2025-06-12 19:06:34 +0200

    changed voiceovers (final), proof table modification

--- zkmarek/video/slides/e7/episode7.py
+++ zkmarek/video/slides/e7/episode7.py
@@ -9,7 +9,7 @@ from zkmarek.video.slides.e7.single_level_verkle import SingleLevelVerkleTree
 from zkmarek.video.slides.e7.three_level_vt import ThreeLevelVerkleTree
 from zkmarek.video.slides.e7.proofs import Proofs
 from zkmarek.video.slides.e7.multiproofs import Multiproofs
-from zkmarek.video.slides.e7.code_multiproofs import Multiproofs as CodeMultiproofs
+# from zkmarek.video.slides.e7.code_multiproofs import Multiproofs as CodeMultiproofs
 from zkmarek.video.slides.e7.final_season1 import FinalSeason1
 
 EPISODE7 = [
@@ -17,22 +17,22 @@ EPISODE7 = [
     #     sound="data/sound/episode3/slide0-0.mp3",
     #     background_sound="data/sound/e7/background.mp3",
     # ),
-    # Sponsored(),
-    # FinalSeason1(),
-    # Recap(),
-    # PreviouslyVectorCommitment(),
-    # FullVideos(),
-    # TitleSlide("Elliptic curves", sound = "data/sound/e7/slide2-0.mp3", 
-    #            wait_time=2, subtitle="in the Verkle trees", wait_time_for_subtitle=1),
-    # EllipticCurves(),
-    # TitleSlide("Verkle trees", sound = "data/sound/e7/slide2-7.mp3", wait_time=2),
-    # SingleLevelVerkleTree(),
-    # TitleSlide("Multi-level Verkle Tree", sound = "data/sound/e7/slide4-0.mp3", wait_time=2),
+    Sponsored(),
+    FinalSeason1(),
+    Recap(),
+    PreviouslyVectorCommitment(),
+    FullVideos(),
+    TitleSlide("Elliptic curves", sound = "data/sound/e7/slide2-0.mp3", 
+               wait_time=2, subtitle="in the Verkle trees", wait_time_for_subtitle=1),
+    EllipticCurves(),
+    TitleSlide("Verkle trees", sound = "data/sound/e7/slide2-7.mp3", wait_time=2),
+    SingleLevelVerkleTree(),
+    TitleSlide("Multi-level Verkle Tree", sound = "data/sound/e7/slide4-0.mp3", wait_time=2),
     ThreeLevelVerkleTree(),
-    # TitleSlide("Proofs", sound = "data/sound/e7/slide5-0.mp3", wait_time=2),
-    # Proofs(),
-    # TitleSlide("Multiproofs", wait_time=2),
-    # Multiproofs(),
+    TitleSlide("Proofs", sound = "data/sound/e7/slide5-0.mp3", wait_time=2),
+    Proofs(),
+    TitleSlide("Multiproofs", wait_time=2),
+    Multiproofs(),
     # TitleSlide("Code", sound = "data/sound/e7/slide6-16.mp3"),
     # CodeMultiproofs(),
 ]
