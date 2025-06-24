da309bcf88918d06ffe9d08149195b527177a20f
martawleklinska <wleklinskam@gmail.com>
2025-06-24 14:15:30 +0200

    added voices to all of the slides except merkle verkle -c ompiling the first half

--- zkmarek/video/slides/e7/episode7.py
+++ zkmarek/video/slides/e7/episode7.py
@@ -5,22 +5,23 @@ from zkmarek.video.slides.e7.watch_other import FullVideos
 from zkmarek.video.slides.common.title import TitleSlide
 from zkmarek.video.slides.e7.elliptic_curves import EllipticCurves
 from zkmarek.video.slides.e7.single_level_verkle import SingleLevelVerkleTree
-from zkmarek.video.slides.e7.three_level_vt import ThreeLevelVerkleTree
-from zkmarek.video.slides.e7.proofs import Proofs
-from zkmarek.video.slides.e7.multiproofs import Multiproofs
-from zkmarek.video.slides.e7.code_multiproofs import Multiproofs as CodeMultiproofs
+# from zkmarek.video.slides.e7.three_level_vt import ThreeLevelVerkleTree
+# from zkmarek.video.slides.e7.proofs import Proofs
+# from zkmarek.video.slides.e7.multiproofs import Multiproofs
+# from zkmarek.video.slides.e7.code_multiproofs import Multiproofs as CodeMultiproofs
 from zkmarek.video.slides.e7.final_season1 import FinalSeason1
-from zkmarek.video.slides.e7.merkle_verkle import MerkleVerkle
-from zkmarek.video.slides.e6.credits import Credits
-from zkmarek.video.slides.common.subscribe import Subscribe
+# from zkmarek.video.slides.e7.merkle_verkle import MerkleVerkle
+# from zkmarek.video.slides.e6.credits import Credits
+# from zkmarek.video.slides.common.subscribe import Subscribe
 
 EPISODE7 = [
     Intro(
-        sound="data/sound/episode3/slide0-0.mp3",
+        sound="data/sound/e7/slide0-0.mp3",
         background_sound="data/sound/e6/background.mp3",
     ),
     Sponsored(),
     FinalSeason1(),
+    TitleSlide("Verkle Trees", wait_time=1.5),
     Recap(),
     FullVideos(),
     TitleSlide("Elliptic curves", sound = "data/sound/e7/slide2-0.mp3", 
@@ -28,16 +29,16 @@ EPISODE7 = [
     EllipticCurves(),
     TitleSlide("Verkle trees", sound = "data/sound/e7/slide2-7.mp3", wait_time=2),
     SingleLevelVerkleTree(),
-    TitleSlide("Multi-level Verkle Tree", sound = "data/sound/e7/slide4-0.mp3", wait_time=2),
-    ThreeLevelVerkleTree(),
-    TitleSlide("Proofs", sound = "data/sound/e7/slide5-0.mp3", wait_time=2),
-    Proofs(),
-    TitleSlide("Multiproofs", wait_time=2),
-    Multiproofs(),
-    TitleSlide("Code"),
-    CodeMultiproofs(),
-    TitleSlide("Merkle vs Verkle Trees", sound = "data/sound/e7/slide8-0.mp3", wait_time=2),
-    MerkleVerkle(),
-    Subscribe("data/sound/episode1/s28.wav"),
-    Credits(),
+    # TitleSlide("Multi-level Verkle Tree", sound = "data/sound/e7/slide4-0.mp3", wait_time=2),
+    # ThreeLevelVerkleTree(),
+    # TitleSlide("Proofs", sound = "data/sound/e7/slide5-0.mp3", wait_time=2),
+    # Proofs(),
+    # TitleSlide("Multiproofs", wait_time=2),
+    # Multiproofs(),
+    # TitleSlide("Code"),
+    # CodeMultiproofs(),
+    # TitleSlide("Merkle vs Verkle Trees", sound = "data/sound/e7/slide8-0.mp3", wait_time=2),
+    # MerkleVerkle(),
+    # Subscribe("data/sound/episode1/s28.wav"),
+    # Credits(),
 ]
